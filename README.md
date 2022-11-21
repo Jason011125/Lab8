604a23ec399194dca35d03712f616e761e7b1a31
Vedant2812 <vgoradia@ucsd.edu>
2022-11-20 17:09:05 -0800

    Explore finished

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Lab 8 - Starter
 Q1:
-I would fit it in option2, as it will automatically test my program and output the test result for some basic cases.
+I would fit it in option1, as it will automatically test my program and output the test result for some basic cases.
 Q2:
 No. Since end to end is used to check whether the whole program is working, not for a particular function.
+Q3:
+No. Unit testing is only used to test the smallest possible fragment of the code, the messaging function in a messaging application is a bigpart of the code for that application
+Q4:
+Yes. The max message length is a very small fragment of the code that we can test, so unit testing would aboslutely work in this case, as a simple true or false test case can help us test this unit of code.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,87 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//Phone number
+test('tests if this is valid phone number', () => {
+    expect(functions.isPhoneNumber('646-510-5725')).toBe(true);
+});
+
+test('tests if this is valid phone number', () => {
+    expect(functions.isPhoneNumber('216-773-2523')).toBe(true);
+});
+
+test('tests if this is invalid phone number', () => {
+    expect(functions.isPhoneNumber(646-510-5725)).toBe(false);
+});
+
+test('tests if this is invalid phone number', () => {
+    expect(functions.isPhoneNumber('6465105725')).toBe(false);
+});
+
+//Email
+test('tests if this is valid Email', () => {
+    expect(functions.isEmail('vedantgoradia119@gmail.com')).toBe(true);
+});
+
+test('tests if this is valid Email', () => {
+    expect(functions.isEmail('vgoradia@ucsd.edu')).toBe(true);
+});
+
+test('tests if this is invalid Email', () => {
+    expect(functions.isEmail('vedantgoradia119gmail.com')).toBe(false);
+});
+
+test('tests if this is invalid Email', () => {
+    expect(functions.isEmail('vgoradia@gmail')).toBe(false);
+});
+
+// //Strong password
+test('tests if this is valid strong password', () => {
+    expect(functions.isStrongPassword('CSE110')).toBe(true);
+});
+
+test('tests if this is valid strong password', () => {
+    expect(functions.isStrongPassword('Prof_Powell')).toBe(true);
+});
+
+test('tests if this is invalid strong password', () => {
+    expect(functions.isStrongPassword('CSE110!')).toBe(false);
+});
+
+test('tests if this is invalid strong password', () => {
+    expect(functions.isStrongPassword('CSE')).toBe(false);
+});
+
+// //Date
+test('tests if this is valid Date', () => {
+    expect(functions.isDate('01/02/2022')).toBe(true);
+});
+
+test('tests if this is valid Date', () => {
+    expect(functions.isDate('1/2/1990')).toBe(true);
+});
+
+test('tests if this is invalid Date', () => {
+    expect(functions.isDate('1/2/90')).toBe(false);
+});
+
+test('tests if this is invalid Date', () => {
+    expect(functions.isDate('13/2/90')).toBe(false);
+});
+
+// //Hex-color
+test('tests if this is valid Hex-color', () => {
+    expect(functions.isHexColor('#666')).toBe(true);
+});
+
+test('tests if this is valid Hex-color', () => {
+    expect(functions.isHexColor('#FFFF76')).toBe(true);
+});
+
+test('tests if this is invalid Hex-color', () => {
+    expect(functions.isHexColor('FFFG76')).toBe(false);
+});
+
+test('tests if this is invalid Hex-color', () => {
+    expect(functions.isHexColor('#FFFFF56')).toBe(false);
+});
